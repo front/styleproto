ea65540fe62934e82b3f56194e5957ccf6ad54b0
Frank Gjertsen <frank.gjertsen@gmail.com>
2014-01-16 11:54:48 +0100

    Add gems - singuraritygs and breakpoint

--- Gruntfile.js
+++ Gruntfile.js
@@ -120,7 +120,7 @@ module.exports = function (grunt) {
     compass: {
       options: {
         // If you're using global Sass gems, require them here.
-        // require: ['singularity', 'jacket'],
+        require: ['singularitygs', 'breakpoint'],
         bundleExec: true,
         sassDir: '<%= yeoman.app %>/_scss',
         cssDir: '.tmp/css',

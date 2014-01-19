202d2832623b18ea1f63f6bf86922667b9849ec2
Frank Gjertsen <frank.gjertsen@gmail.com>
2014-01-19 18:27:53 +0100

    Configure basic tasks for compass, jekyll and browser-sync

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,7 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-    // Configurable paths
-    path: {
-      app: 'app',
-      dist: 'dist'
-    },
-
+    
     jekyll: {
       build: {
         dest: '_site'
@@ -18,19 +13,80 @@ module.exports = function (grunt) {
     },
 
     compass: {
+      options: {
+        // If you're using global Sass gems, require them here.
+        require: ['breakpoint', 'singularitygs'],
+        bundleExec: true,
+        relativeAssets: true,
+        environment: 'development',
+        sassDir: '_sass',
+        fontsDir: 'fonts'
+
+      },
       dist: {
-        config: {
-          sassDir: '_sass',
-          cssDir: '.tmp/css'
+        options: {
+          cssDir: 'css'
+        }   
+      },
+      site: {
+        options: {
+          cssDir: '_site/css'
+        }
+      }
+    },
+
+    watch: {
+      dist: {
+        files: '_sass/**/*.scss',
+        tasks: ['compass:dist']
+      },
+      site: {
+        files: '_sass/**/*.scss',
+        tasks: ['compass:site'],
+        options: {
+          spawn: false
+        }
+      },
+      jekyll: {
+        files: ['_layouts/*.html', 'css/*.css'],
+        tasks: ['jekyll']
+      }
+    },
+
+    browser_sync: {
+      files: {
+        src: ['_site/css/*.css'],
+      },
+      options: {
+        watchTask: 'true',
+        ghostMode: {
+          clicks: true,
+          scroll: true,
+          links: true,
+          forms: true
+        },
+        server: {
+          baseDir: '_site'
         }
       }
     }
   });
 
   // Define Tasks
+  grunt.registerTask('build', [
+    'compass',
+    'jekyll'
+  ]);
+
+  grunt.registerTask('site', [
+    'build',
+    'browser_sync',
+    'watch:site'
+  ]);
+
   grunt.registerTask('default', [
-    'check',
-    'test',
-    'build'
+    'build',
+    'browser_sync',
+    'watch:dist'
   ]);
 };

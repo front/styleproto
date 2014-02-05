b9d5da9f26727b6f2fbc4e335293344bf8197acb
Frank Gjertsen <frank.gjertsen@gmail.com>
2014-02-05 11:45:15 +0100

    Add ui iconfont and animate flyout menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
   // Toggle primary navigation
-  $('.nav-main-toggle').on('click', function(e) {
-    e.preventDefault();
+  var menuIcon = 'm';
+  
+  $('.nav-main-toggle').on('click', function( event ) {
+    var $this = $(this);
+    event.preventDefault();
+
+    // Toggle class
     $('body').toggleClass('active-nav');
+    
+    // Changes icon
+    if ( menuIcon == 'm' ) {
+      $this.attr('data-icon', 'x');
+      menuIcon = 'x';
+      console.log(menuIcon);
+    } else {
+      $this.attr('data-icon', 'm');
+      menuIcon = 'm';
+      console.log(menuIcon);
+
+    } 
   });
 });

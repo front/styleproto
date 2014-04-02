5c8aab215c85f2a06294144e16c67d30461a40cd
Frank Gjertsen <frank.gjertsen@gmail.com>
2014-04-02 13:24:11 +0200

    Add large transparent main-nav exit btn

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
-$(document).ready(function() {
+(function($) {
   // Toggle primary navigation
-  var menuIcon = 'm';
+  var menuIcon = 'm',
+      $menuBtn = $('.nav-main-toggle'),
+      $body = $('body');
   
-  $('.nav-main-toggle').on('click', function( event ) {
-    var $this = $(this);
+  $menuBtn.on('click', function( event ) {
     event.preventDefault();
 
     // Toggle class
-    $('body').toggleClass('active-nav');
-    
+    $body.toggleClass('active-nav');
+    toggleMenuBtn();
+  });
+
+
+  // Close main menu
+  $('.nav__main--exit').on('click', function(e) {
+    e.preventDefault();
+    $body.removeClass('active-nav');
+    toggleMenuBtn();
+  });
+  
+  function toggleMenuBtn() {
     // Changes icon
     if ( menuIcon == 'm' ) {
-      $this.attr('data-icon', 'x');
+      $menuBtn.attr('data-icon', 'x');
       menuIcon = 'x';
-      console.log(menuIcon);
     } else {
-      $this.attr('data-icon', 'm');
+      $menuBtn.attr('data-icon', 'm');
       menuIcon = 'm';
-      console.log(menuIcon);
-
     } 
-  });
-});
+  }
+
+})(jQuery);
